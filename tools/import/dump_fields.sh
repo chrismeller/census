7bf9da314d7500405099713fb9ac265826bd8b77
Chris Meller <chris@doesnthaveone.com>
2012-07-12 16:12:10 -0400

    Totally revamp the field dump script, which was added too quickly anyway.

--- tools/import/dump_fields.sh
+++ tools/import/dump_fields.sh
@@ -1,8 +1,18 @@
 # iterate over all the directories in the current directory
 for dir in `find . -maxdepth 1 -type d ! -name .`
 do
-	# dump the fields from the first dbf file in their extracted directory into a fields.txt file
-	dbf_dump --info $(ls $dir/extracted/*.dbf | head -1) > $dir/fields.txt
+
+	# what is the first dbf file in the directory?
+	first_file=$(ls $dir/extracted/*.dbf | head -1)
+
+	# what is the base name of that file? it's what dbf_dump uses as the table name
+	file_name=$(echo "$first_file" | sed -re 's/\S+\/(\w+).dbf/\1/' )
+
+	# what should the table be named? - based on the directory name, removes the leading ./ and lowercases it
+	table=$(echo "$dir" | sed -re 's/\.\///' | tr '[:upper:]' '[:lower:]')
+
+	# and finally dump the sql statement into something like place.sql, translating the table name on the way
+	dbf_dump --info --sql $first_file | sed -re 's/'"$file_name"'/'"$table"'/' > $table.sql
 	
-	echo "Saved $dir/fields.txt"	
+	echo "Saved $table.sql"
 done
