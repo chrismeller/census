2c4ecf2b156e3c2cc5a02d10235a2eada145f58c
Chris Meller <chris@doesnthaveone.com>
2012-07-13 11:17:12 +0000

    Stupid f'ing git. Fix a merge error that got committed verbatum.

--- tools/import/load_schema.sh
+++ tools/import/load_schema.sh
@@ -2,9 +2,5 @@
 for file in *.sql
 do
 	# load them all into ../census.db
-<<<<<<< HEAD
 	echo ".read $file" | sqlite3 ../census.db
-=======
-	sqlite3 -init $file ../census.db
->>>>>>> dfff79e704146c0f504e41787d6f3a94ea7fcbbb
 done
